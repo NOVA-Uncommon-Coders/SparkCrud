e4c8240af7a725e51f8288d23ce13a34408b9133
Steve Jobs <peter@Steves-MBP.home>
2017-03-05 12:26:30 -0500

    containsKey or nameEquals(name)?

--- src/com/theironyard/novauc/Main.java
+++ src/com/theironyard/novauc/Main.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 
 public class Main {
 
+    public static HashMap<String, User> globalAccounts = new HashMap<>();
+
     public static void main(String[] args) {
 
         Spark.init();
@@ -16,6 +18,48 @@ public class Main {
             String name = request.attribute("userName");
 
             HashMap local = new HashMap();
-        }));
+            //if username
+            //if ()
+            return "";
+        });
+
+        Spark.post("/create-user", (request, response) -> {
+            String name = request.queryParams("createUser");
+            String password = request.queryParams("createPassword");
+
+            Session session = request.session();
+
+            if (!name.equals(globalAccounts.get(name))) {
+                globalAccounts.put(name, new User(name, password));
+                session.attribute("createUser", name);
+                //should createUser on the line above, actually be userName?
+            }
+            else {
+                if(name.equals(globalAccounts.get(name))) {
+                    response.redirect("/login");
+                }
+            }
+            response.redirect("/");
+            return "";
+        });
+
+        Spark.post("/login", (request, response) -> {
+            String name = request.queryParams("userLogin");
+            String password = request.queryParams("passwordLogin");
+            Session session = request.session();
+
+            //if(name.equals(globalAccounts.get(name)))
+            if(globalAccounts.containsKey(name)) {
+                if(password.equals(globalAccounts.get(name).getPassword())) {
+                    session.attribute("userLogin", name);
+
+                }
+            }
+            else {
+                return "If you would like to create an account, then create an account";
+            }
+            response.redirect("/");
+            return "It's your fault";
+        });
     }
 }
--- src/com/theironyard/novauc/User.java
+++ src/com/theironyard/novauc/User.java
@@ -2,4 +2,20 @@ package com.theironyard.novauc;
 
 
 public class User {
+    private String name;
+    private String password;
+
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    String getPassword() {
+        return password;
+    }
 }
