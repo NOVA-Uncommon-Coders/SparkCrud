58d7c56d87e1e25521bb14f17ecae148162e70ba
D'Angelo Joyce <dangelomjoyce@gmail.com>
2017-03-06 08:57:29 -0500

    SparkCrud

---
+++ src/com/theironyard/novauc/ComedyAlbum.java
@@ -0,0 +1,23 @@
+package com.theironyard.novauc;
+
+/**
+ * Created by dangelojoyce on 3/2/17.
+ */
+public class ComedyAlbum {
+
+    String author;
+    String albumName;
+    String genre;
+    int duration;
+
+
+    public ComedyAlbum(String author, String albumName, String genre, int duration){
+        this.author = author;
+        this.albumName = albumName;
+        this.genre = genre;
+        this.duration = duration;
+
+
+    }
+
+}
