a742484bb08d0fd46e57ea6bfe3310e33c5ec1e8
Steve Jobs <peter@steves-mbp.nvstu.edu>
2017-03-06 20:31:01 -0500

    commit 9

--- src/com/theironyard/novauc/Entry.java
+++ src/com/theironyard/novauc/Entry.java
@@ -2,34 +2,13 @@ package com.theironyard.novauc;
 
 
 public class Entry {
-    private int id;
-    private int replyId;
-    private String author;
-    private String text;
+    int id;
+    String author;
+    String text;
 
-    private static int incrementer = 0;
-    //public final int identifier;
-
-    public Entry(int id, int replyId, String author, String text) {
+    public Entry(int id, String author, String text) {
         this.id = id;
-        this.replyId = incrementer;
         this.author = author;
         this.text = text;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public int getReplyId() {
-        return replyId;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getText() {
-        return text;
     }
 }
--- src/com/theironyard/novauc/Main.java
+++ src/com/theironyard/novauc/Main.java
@@ -19,7 +19,7 @@ public class Main {
         Spark.init();
 
         Spark.get("/", ((request, response) -> {
-            System.out.println("got into /");
+            //System.out.println("got into /");
             Session session = request.session();
             String name = session.attribute("userName");
 
@@ -27,13 +27,15 @@ public class Main {
             ArrayList storage = new ArrayList();
 
             if(!accountInfo.containsKey(name)) {
-                System.out.println("1");
+                //System.out.println("1");
                 return new ModelAndView(userActivity,"index.html");
             }
             else {
-                System.out.println("2");
+                //System.out.println("2");
                 userActivity.put("entries", entries);
                 userActivity.put("userName", name);
+                //userActivity.put("messageID", entries.size());
+                System.out.println("currently in userActivity, the entry is at " + entries.size());
                 return new ModelAndView(userActivity, "tracker.html");
             }
         }),
@@ -41,7 +43,7 @@ public class Main {
         );
 
         Spark.post("/getIn", (request, response) -> {
-            System.out.println("got into /getIn");
+            //System.out.println("got into /getIn");
             accountInfo.put("default", new User("default", "d"));
             String name = request.queryParams("userLogin");
             String password = request.queryParams("passwordLogin");
@@ -65,12 +67,9 @@ public class Main {
             Session session = request.session();
             String name = session.attribute("userName");
             String message = request.queryParams("newEntry");
-            String messageId = request.queryParams("messageId");
-
-            int IdForMessage =
-
-            Entry entryObj = new Entry(entries.size(), replyId, name, message);
+            Entry entryObj = new Entry(entries.size(), name, message);
             //System.out.println(entryObj);
+            //session.attribute("messageID", entries.size());
             entries.add(entryObj);
             response.redirect("/");
             return "";
@@ -78,8 +77,20 @@ public class Main {
 
         Spark.post("/delete-message", (request, response) -> {
            Session session = request.session();
-           String name = session.attribute("userName");
-           entries.remove(entries.size()-1);  //removes only the latest post
+           String delete = request.queryParams("messageID");
+           System.out.println(delete);
+
+
+
+
+
+            //String name = session.attribute("userName");
+           //session.attribute("messageID", entries.size());
+           //String deletede = session.attribute("messageID");
+           //entries.remove(Integer.valueOf(delete));  //removes only the latest post
+           //get the number from messageID, then match it up against the
+            // index in entries, -1
+           //entries.remove(Integer.valueOf(delete));
            response.redirect("/");
            return "";
         });
