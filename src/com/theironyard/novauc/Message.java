3c455811fe0d64e4f4113c2c29f54f1017a31f5f
Octavio Cisneros <octavio@Octavios-MacBook-Air.local>
2017-03-11 13:48:29 -0500

    version1

---
+++ src/com/theironyard/novauc/Message.java
@@ -0,0 +1,15 @@
+package com.theironyard.novauc;
+
+
+public class Message {
+
+    int id;
+    String author;
+    String tweet;
+
+    public Message(int id, String author, String tweet) {
+        this.id = id;
+        this.author = author;
+        this.tweet = tweet;
+    }
+}
